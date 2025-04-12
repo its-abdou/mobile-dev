70d73885150d7674b1ec6fa14f29a30934c9faf8
Abdessamed <abdessamed630@gmail.com>
2025-04-12 18:00:15 +0100

    TP2-exo2

--- app/src/main/java/com/example/mob_dev_tp/MainActivity.java
+++ app/src/main/java/com/example/mob_dev_tp/MainActivity.java
@@ -1,43 +1,33 @@
 package com.example.mob_dev_tp;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-   Button incrementButton;
-   Button decreaseButton;
-   TextView text1;
-   int counter = 0;
+  Button button;
+   TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        incrementButton = findViewById(R.id.incrementButton);
-        decreaseButton = findViewById(R.id.decrementButton);
-        text1 = findViewById(R.id.text);
-        decreaseButton.setOnClickListener(new View.OnClickListener() {
+        button = findViewById(R.id.button);
+        text = findViewById(R.id.text);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (counter<= 0){
-                    decreaseButton.setEnabled(false);
-                }
-                else{
-                    counter --;
-                    text1.setText("Vous avez "+counter+" points actuellement");}
-
-                }
+                text.setText("Hello, World!");
+                Toast MsgToast = Toast.makeText(MainActivity.this, "message à afficher", Toast.LENGTH_SHORT);
+                MsgToast.setGravity(Gravity.CENTER, 0,0);
+                MsgToast.show();
+            }
         });
-        };
+    };
 
-    public void increment(View view) {
-        counter++;
-        if (counter > 0) {
-            decreaseButton.setEnabled(true);
-        }
-        text1.setText("Vous avez "+counter+" points actuellement");
-    }
-    }
+
+}
