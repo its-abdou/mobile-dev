d59626c103ade16cf3610646494bdae6e0426b60
Abdessamed <abdessamed630@gmail.com>
2025-04-12 18:09:42 +0100

    TP5-exo2 (adjust)

--- app/src/main/java/com/example/mob_dev_tp/MainActivity.java
+++ app/src/main/java/com/example/mob_dev_tp/MainActivity.java
@@ -4,11 +4,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
