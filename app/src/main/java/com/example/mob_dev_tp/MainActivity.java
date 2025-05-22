fbc34a8d8fc865319edb4ad69b45d10412aacdf8
Abd Essamed <abdessamed630@gmail.com>
2025-05-22 21:49:41 +0100

    TP8-exo2

--- app/src/main/java/com/example/mob_dev_tp/MainActivity.java
+++ app/src/main/java/com/example/mob_dev_tp/MainActivity.java
@@ -1,49 +1,71 @@
 package com.example.mob_dev_tp;
 
-import android.content.Intent;
-import android.media.MediaPlayer;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    MediaPlayer sound;
+  ProgressBar progressBar;
+  TextView progressTxt;
+    boolean enExecution = false;
+    int i = 0;
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            i++;
+            progressTxt.setText(10*i+"%");
+            progressBar.incrementProgressBy(10);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sound = MediaPlayer.create(getApplicationContext(),R.raw.accueil);
-        sound.start();
+        progressBar = findViewById(R.id.progressBar);
+        progressTxt = findViewById(R.id.progressTxt);
+
+
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
 
         Thread thread = new Thread() {
             public void run() {
                 try {
-                    Thread.sleep(10000);
+                    for (int i = 0;enExecution && i < 10; i++) {
+                        Thread.sleep(1000);
+                        Message msg = handler.obtainMessage();
+                        handler.sendMessage(msg);
+                    }
+
 
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
-                } finally {
-                    Uri uri = Uri.parse("https://www.google.com/");
-                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                    startActivity(intent);
                 }
-
-
             }
         };
-             thread.start();
-        };
-
-
+        enExecution =true;
+        thread.start();
+    }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        sound.release();
+    protected void onStop() {
+        super.onStop();
+        enExecution = false;
+
     }
 }
 
